9ffefd9f5902777a8b300c5b3e4da634f549fb92
girz298-home <girz298@gmail.com>
2018-05-05 02:13:03 +0300

    Enable debug pack. Make db_provider works.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,7 +4,17 @@ namespace App\Repository;
 
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserRepository extends EntityRepository
+class UserRepository extends EntityRepository implements UserLoaderInterface
 {
+    public function loadUserByUsername($username)
+    {
+        return $this
+            ->createQueryBuilder('u')
+            ->where('u.username = :username')
+            ->setParameter(':username', $username)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
